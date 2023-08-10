98001f171b74e5d02f9d7a2449acd101479b21b9
(Trevor) Duygun Beydag <Duygun.Beydag@proagrica.com>
2023-08-10 15:17:55 +0100

    git test

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "APIBaseSPM",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        
         .library(
             name: "APIBaseSPM",
             targets: ["APIBaseSPM"]),
